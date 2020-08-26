f3a61532933ce5d172c7b1c08eb58987fd24932e
alejandro <20160338@ce.pucmm.edu.do>
2020-08-26 10:41:39 -0400

    Agregando cambios faltantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cliente-rest';
+
+  constructor( private router: Router){
+    const token = localStorage.getItem("token");
+    console.log("AppComponent 00");
+
+    if(token === null || token === undefined){
+      console.log("AppComponent 11");
+      this.router.navigate(["login"]);
+    }
+    console.log("AppComponent 22");
+  }
 }
